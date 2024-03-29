c53c97ebb864986a1c39d06eaed5b11e5c05c221
AmonMunyai <60216128+AmonMunyai@users.noreply.github.com>
2024-03-30 00:17:48 +0200

    chore: boilerplate entry point for cli

--- src/myalx/cli.py
+++ src/myalx/cli.py
@@ -1,34 +1,47 @@
-import sys
-
 import click
-
-from .spider import run_spider
-from .utils import AlxProject
 
 
 @click.group()
-def main():
+@click.pass_context
+def cli(ctx):
     """Main entry point for myALX."""
     pass
 
 
-@main.command()
-@click.argument("project_url", type=str)
-def startproject(project_url):
+@cli.command()
+@click.argument("name")
+def genfile(name):
+    """Generate new file using pre-defined templates."""
+    pass
+
+
+@cli.command()
+@click.argument("task")
+def runchecker(task):
+    """Run a self-contained checker."""
+    pass
+
+
+@cli.command()
+def settings():
+    """Get settings values."""
+    pass
+
+
+@cli.command()
+@click.argument("url")
+@click.argument("dir", required=False)
+@click.pass_context
+def startproject(ctx, url, dir):
     """Create a new project."""
-    try:
-        # TODO: make email and password retrieval more secure
-        email = input("Email: ")
-        password = input("Password: ")
+    pass
 
-        scraped_data = run_spider(project_url, email, password)
 
-        project = AlxProject(scraped_data)
-        project.start()
-
-    except Exception as e:
-        raise e
+@cli.command()
+def version():
+    """Print myALX version."""
+    pass
 
 
 if __name__ == "__main__":
-    main()
+    cli()
