f2875bbe8e26a53955fc1d1c4498c22e832cc64f
AmonMunyai <60216128+AmonMunyai@users.noreply.github.com>
2024-03-30 00:09:45 +0200

    feat: remove config module

--- src/myalx/cli.py
+++ src/myalx/cli.py
@@ -2,7 +2,6 @@ import sys
 
 import click
 
-from .config import AlxConfig
 from .spider import run_spider
 from .utils import AlxProject
 
@@ -11,40 +10,6 @@ from .utils import AlxProject
 def main():
     """Main entry point for myALX."""
     pass
-
-
-@main.command()
-@click.argument("section_key", type=str)
-@click.argument("value", type=str, required=False)
-def config(section_key, value):
-    """Configure settings for myALX."""
-    try:
-        section, key = section_key.split(".")
-
-    except ValueError:
-        msg = "key does not contain a section: {}".format(section_key)
-        raise click.ClickException(msg)
-
-    if not key:
-        msg = "key does not contain a variable name: {}".format(section_key)
-        raise click.ClickException(msg)
-
-    alx_config = AlxConfig()
-
-    try:
-        if value is not None:
-            alx_config.set(section, key, value)
-
-        else:
-            value = alx_config.get(section, key)
-
-            if value is None:
-                sys.exit(1)
-
-            click.echo(value)
-
-    except Exception as e:
-        raise e
 
 
 @main.command()
